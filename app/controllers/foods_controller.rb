1b38305df4d7684f94947f8f5aaf63b5acb79e57
naricrow <nari.crow.city@gmail.com>
2024-01-09 21:54:15 +0900

    トップページ表示のみ実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,26 @@
 class FoodsController < ApplicationController
   
   def index
+    @foods = Food.all
   end
-  
+
+  def new
+    @food =Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to '/'
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:image, :name, :content).merge(user_id: current_user.id)
+  end
+
 end
